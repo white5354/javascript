7cdb699607b0ae44d7b0f6273f360a7787d5a020
baiyun8 <baiyun8@ceair.com>
2020-12-09 00:08:43 +0800

    登录界面

--- angularjs/1.js
+++ angularjs/1.js
@@ -1,10 +1,4 @@
-function de () {
-    var s = {
-        name : '你还',
-        age: "12",
-        scholle: "我是哦"
-    }
-    console.log(JSON.parse(JSON.stringify(s)) );
-    
+function de() {
+    console.log(de);
+    得不  
 }
-de()
